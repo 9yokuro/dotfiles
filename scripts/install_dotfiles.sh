af1705dbda44bc270d3c1d1a34868148f534cc51
9yokuro <119095935+9yokuro@users.noreply.github.com>
2024-06-30 11:58:18 +0900

    feat(scripts): update

--- scripts/install_dotfiles.sh
+++ scripts/install_dotfiles.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
 
 DOTFILES_ROOT="$(git rev-parse --show-toplevel)" || exit 1
+DOTFILES_DATA="${XDG_DATA_HOME:-${HOME}/.local/share}/dotfiles"
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
 
-for path in "aqua" \
-  "foot" \
-  "mako" \
-  "sway" \
-  "waybar"
+for path in "foot/" "sway/"
 do
-  ln --symbolic --verbose "${DOTFILES_ROOT}/${path}" "${XDG_CONFIG_HOME}/${path}"
+  ln --symbolic --verbose "${DOTFILES_ROOT}/${path}" "${XDG_CONFIG_HOME}"
 done
 
 for path in ".bash_profile" \
   ".bashrc" \
   ".gitconfig" \
   ".inputrc" \
-  ".vim" \
-  "wallpapers"
+  ".vim/"
 do
-  ln --symbolic --verbose "${DOTFILES_ROOT}/${path}" "${HOME}/${path}"
+  ln --symbolic --verbose "${DOTFILES_ROOT}/${path}" "${HOME}"
 done
+
+ln --symbolic --verbose "${DOTFILES_ROOT}/wallpapers/" "${DOTFILES_DATA}"
 
 ICONS_DIR="${HOME}/.icons/default"
 mkdir --parents --verbose "${ICONS_DIR}"
